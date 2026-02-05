456b8c8c0447a515d62551c7c28ed6a1e7488088
AndrewStone <aaandrewww666@mail.ru>
2026-02-05 03:56:12 +0300

    [mod] Added solution structure with three layers: Presentation (Avalonia project), Infrastructure and Domain

---
+++ RSTechTestApplication.Presentation/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RSTechTestApplication.Presentation.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
