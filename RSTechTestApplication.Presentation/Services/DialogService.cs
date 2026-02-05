b89b6c454b7c09bf1fda4263fa817661d194236e
AndrewStone <aaandrewww666@mail.ru>
2026-02-05 05:52:09 +0300

    [mod] Added dialog service to create dialog windows via viewmodel.

---
+++ RSTechTestApplication.Presentation/Services/DialogService.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Controls;
+using RSTechTestApplication.Presentation.ViewModels;
+using RSTechTestApplication.Presentation.Views;
+using System;
+using System.Threading.Tasks;
+
+namespace RSTechTestApplication.Presentation.Services
+{
+    public class DialogService : IDialogService
+    {
+        private readonly Window _window;
+
+        public DialogService(Window window)
+        {
+            _window = window;
+        }
+
+        public async Task<bool> ShowDialogAsync<TViewModel>(TViewModel viewModel)
+            where TViewModel : class
+        {
+            var dialog = CreateDialog(viewModel);
+            dialog.DataContext = viewModel;
+            return await dialog.ShowDialog<bool>(_window);
+        }
+
+        private static Window CreateDialog<TViewModel>(TViewModel viewModel)
+        {
+            return viewModel switch
+            {
+                TaskItemViewModel => new AddEditTaskWindow(),
+                _ => throw new ArgumentException($"No dialog for {typeof(TViewModel).Name}")
+            };
+        }
+    }
+}
---
+++ RSTechTestApplication.Presentation/Services/IDialogService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace RSTechTestApplication.Presentation.Services
+{
+    public interface IDialogService
+    {
+        Task<bool> ShowDialogAsync<TViewModel>(TViewModel viewModel) where TViewModel : class;
+    }
+}
