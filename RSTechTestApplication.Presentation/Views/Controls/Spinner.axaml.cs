8adafd05a7f3d1f8ee13cafb854f0e6dadcdc4f6
AndrewStone <aaandrewww666@mail.ru>
2026-02-05 06:11:12 +0300

    [mod] A spinner control has been added to show the user whether data is being loaded

---
+++ RSTechTestApplication.Presentation/Views/Controls/Spinner.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RSTechTestApplication.Presentation;
+
+public partial class Spinner : UserControl
+{
+    public Spinner()
+    {
+        InitializeComponent();
+    }
+}
